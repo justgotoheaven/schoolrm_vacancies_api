a5c7e4e64b74efe78a2e3cfa1f324710c499696a
Maxim Ermolaev <ermolaev.me@jhvn.tk>
2022-07-12 11:17:08 +0300

    allow CORS

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from endpoints import cities, schools, vacancies
 import uvicorn
 
 api = FastAPI(title='API вакансий schoolrm.ru',
               version=0.1)
+
+origins = ["*"]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # endpoints
 api.include_router(cities.router, tags=['Районы'], prefix='/cities')
@@ -13,5 +24,5 @@ api.include_router(vacancies.router, tags=['Вакансии'], prefix='/vacanci
 
 if __name__ == '__main__':
     uvicorn.run('api:api',
-                port=8080,
+                port=8888,
                 reload=True)
